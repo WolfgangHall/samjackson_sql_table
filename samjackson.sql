60357c9e735d09c1effbf1452bff3dc928221880
Johall <johnalexander.w.hall@gmail.com>
2016-01-27 19:15:34 -0500

    initial mysql commit

---
+++ samjackson.sql
@@ -0,0 +1,7 @@
+ CREATE DATABASE MOVIES;
+
+ USE MOVIES;
+
+ CREATE TABLE samuel_l_jackson_movies(movies CHAR(50), role CHAR(20), Year INT);
+
+ INSERT INTO samuel_l_jackson_movies (movies, role, Year) VALUES ('Jackie Brown', 'Ordell Robbie', 1997), ('Shaft', 'John Shaft', 2000), ('Snakes on a Plane', 'Neville Flynn', 2006), ('Star Wars: The Clone Wars', 'Mace Windu', 2008), ('Captain America: The First Avenger', 'Nick Fury', 2011), ('Fury', 'Foley', 2012), ('The Avengers', 'Nick Fury', 2012), ('Django Unchained', 'Stephen', 2012), ('Avengers: Age of Ultron', 'Nick Fury', 2015);
